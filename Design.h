d2c40c8788dc3862036480173ec981d0770347df
Kinzokuzu <sauljcervantes2@gmail.com>
2023-03-30 11:21:21 -0700

    Correction to program requirements

--- Design.h
+++ Design.h
@@ -2,17 +2,20 @@
  * 
  * Given a set of students, S, and a set of groups, G, with 0 to 5 students NOT belonging to S,
  * assigned to each group, randomly assign the students belonging to S to the groups belonging to G so
- * that every student belongs to a group and no group has more than 5 members.
+ * that every student belongs to a group and no group has more than 5 members, and return a list of
+ * student in set S and which group they have been assigned to.
  */
 
 /* Data
  *
  * Students (S)
  *  Fields: • identifier (some integer number from 0 to n, where n is the number of students - 1)
+ *          • class section identifier (11 AM or 12 PM)
  *  Structure: array, vector, or similar
  *
  * Groups (G)
  *  Fields: • identifier (some integer number from 0 to n, where n is the number of groups - 1)
+ *          • class section identifier (11 AM for 12 PM)
  *          • number of members assigned to a particular group
  *  Structure: list of key, value pairs (map)
  */
