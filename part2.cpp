7acccb1226ff5fdc19fad97a33731a391ceaf352
Sidney <sid.14boys@yahoo.fr>
2023-04-03 14:55:37 -0700

    Part 2 fixed

--- part2.cpp
+++ part2.cpp
@@ -6,21 +6,26 @@
 #include <unordered_map>
 #include <vector>
 
-struct Student {
+struct Student
+{
   int id = -1;
   int group = -1;
 }; // End Student
-// FIX-ME: When there are more students than available spots in groups, function loops forever.
-std::vector<Student> AssignStudents(std::vector<Student>& s, std::unordered_map<int, int>& g) {
+
+// randomly assigns students to groups with a maximum of five students per group.
+std::vector<Student> AssignStudents(std::vector<Student> &s, std::unordered_map<int, int> &g)
+{
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> distrib(0, g.size() - 1);
 
   int rand_num;
-  for (int i = 0; i < s.size();) {
+  for (int i = 0; i < s.size();)
+  {
     rand_num = distrib(gen);
 
-    if (g[rand_num] < 5) {
+    if (g[rand_num] < 5)
+    {
       s.at(i).group = rand_num; // Assign student i to group rand_num
       g[rand_num]++;            // Increment the number of students in group rand_num
       i++;                      // Move onto next student
@@ -28,17 +33,40 @@ std::vector<Student> AssignStudents(std::vector<Student>& s, std::unordered_map<
     // Else produce another rand_num and try again
   }
 
+  // Assign remaining students to groups with less than 5 members
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s.at(i).group == -1) // Check if student is unassigned
+    {
+      for (int j = 0; j < g.size(); j++)
+      {
+        if (g[j] < 5) // Check if group has less than 5 members
+        {
+          s.at(i).group = j; // Assign student to group j
+          g[j]++;            // Increment number of students in group j
+          break;             // Move onto next unassigned student
+        }
+      }
+    }
+  }
+
   return s;
 }
 
-void PrintStudents(const std::vector<Student>& s) {
-  for (const auto& student : s) {
+// print student ID and his group
+void PrintStudents(const std::vector<Student> &s)
+{
+  for (const auto &student : s)
+  {
     std::cout << "Student: " << student.id << " Group: " << student.group << std::endl;
   }
 }
 
-void PrintGroup(const std::unordered_map<int, int> g) {
-  for (const auto& [key, value] : g) {
+// print group index and he numbers of student inside the group
+void PrintGroup(const std::unordered_map<int, int> g)
+{
+  for (const auto &[key, value] : g)
+  {
     std::cout << "Group: " << key << " # of Students: " << value << std::endl;
   }
 }
